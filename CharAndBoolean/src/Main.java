7fa0cd77d83b57f2c4eb4e471456d9dc63c1eb2d
Arpan Abhishek <arpanforbusiness@gmail.com>
2019-10-06 19:47:38 +0530

    completed the primitive data types

---
+++ CharAndBoolean/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        char theta = '\u03F4';
+        char pi = '\u03C0';
+        System.out.println(theta);
+        System.out.println(pi);
+        boolean isThisTrue = true;
+    }
+}
