b402105ef9867eb3886138aeeca60dce9ad4a75a
Arpan Abhishek <arpanforbusiness@gmail.com>
2019-10-19 16:47:45 +0530

    Completed Operator challenge

--- Operators/src/OperatorChallenge.java
+++ Operators/src/OperatorChallenge.java
@@ -1,2 +1,13 @@
 public class OperatorChallenge {
+    public static void main(String[] args) {
+        double number1 = 20.0;
+        double number2 = 80.0;
+        double addAndMultiply = (number1 + number2) * 100;
+        double remainder = addAndMultiply%40;
+        boolean checkRemainder = remainder == 0 ? true :  false;
+        System.out.println(checkRemainder);
+        if (!checkRemainder){
+            System.out.println("Got some remainder");
+        }
+    }
 }
